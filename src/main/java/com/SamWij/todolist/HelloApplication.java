4fb6e72db53773e8631b016f5a408db2276e712e
Samith <samithmw1000@gmail.com>
2025-07-12 17:01:25 +0530

    TodoList

--- src/main/java/com/SamWij/todolist/HelloApplication.java
+++ src/main/java/com/SamWij/todolist/HelloApplication.java
@@ -20,4 +20,9 @@ public class HelloApplication extends Application {
 	public static void main(String[] args) {
 		launch();
 	}
+
+	@Override
+	public void stop() throws Exception {
+		super.stop();
+	}
 }
